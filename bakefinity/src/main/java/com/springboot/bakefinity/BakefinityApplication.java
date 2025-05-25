1b0fca455c53a013437ecd5cdb95dc90ffed84ec
AhmedAbdElAleem01 <ahmadmu.abdelaleem@gmail.com>
2025-05-25 12:58:55 +0300

    initial structure for backend module

---
+++ bakefinity/src/main/java/com/springboot/bakefinity/BakefinityApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.bakefinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakefinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakefinityApplication.class, args);
+	}
+
+}
---
+++ bakefinity/src/test/java/com/springboot/bakefinity/BakefinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bakefinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakefinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
