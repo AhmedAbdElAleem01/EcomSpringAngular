a3418d6982e3e31809889fdd346eca859e402ce3
khaled1150 <khaledshabn1190@gmail.com>
2025-06-07 21:49:32 +0300

    adding controller to get the product by id

--- bakefinity/src/main/java/com/springboot/bakefinity/controllers/ShopController.java
+++ bakefinity/src/main/java/com/springboot/bakefinity/controllers/ShopController.java
@@ -10,10 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -34,6 +31,11 @@ public class ShopController {
     public ResponseEntity<List<ProductDTO>> getAllProducts(){
         List<ProductDTO> allProducts = productService.getAllProducts();
         return createOkResponse(allProducts);
+    }
+    @GetMapping("/products/{id}")
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable int id){
+        ProductDTO Product = productService.getProductById(id);
+        return createOkResponse(Product);
     }
 
     @GetMapping("/categories")
